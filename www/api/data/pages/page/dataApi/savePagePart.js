650564f4c845f5de905e912f3b9ee549ee68dce6
Ben Peachey <ben@muze.nl>
2023-05-26 17:20:07 +0200

    Change index.html, generated.html and components to match.

--- www/api/data/pages/page/dataApi/savePagePart.js
+++ www/api/data/pages/page/dataApi/savePagePart.js
@@ -1,8 +1,5 @@
 function(component, part, contents) {
-  simplyDataApi.deletePart("pages/" + component, part) // FIXME: this allows the old structure to be updated. should be removed when no longer needed.
-    .then(function() {
-    return simplyDataApi.savePart("pages/" + component, part, contents);
-  })
+  return simplyDataApi.savePart("pages/" + component, part, contents)
     .then(function(response) {
     if (response.status === 200) {
       return response.json();
