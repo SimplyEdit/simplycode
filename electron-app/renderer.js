defa7cd9340c133cc96048cb035e848da0ab9294
govert@muze.nl <govert@muze.nl>
2024-05-17 15:38:14 +0200

    monkey patch complete, handling put http request soon

--- electron-app/renderer.js
+++ electron-app/renderer.js
@@ -1,4 +1,7 @@
+const { protocol } = require("electron")
+
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 
 console.log("Success!")
+
--- www/api/index.js
+++ www/api/index.js
@@ -1,6 +1,8 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, net, protocol, session } = require('electron')
 const path = require('node:path')
+const url = require('url')
 const data = []
+const fs = require('fs')
 
 // https://github.com/sindresorhus/electron-main-fetch
 
@@ -16,7 +18,8 @@ const createWindow = () => {
       }
     })
   
-    win.loadFile('../www/index.html')
+    win.loadURL('simplycode://index.html')
+    //win.loadFile('../www/index.html')
 }
 
 function readRecursive(path) {
@@ -82,128 +85,74 @@ function tagId() {
 
 app.commandLine.appendSwitch('disable-site-isolation-trials');
 
+protocol.registerSchemesAsPrivileged([
+    {
+      scheme: 'simplycode',
+      privileges: {
+        standard: true,
+        secure: true,
+        supportFetchAPI: true
+      }
+    }
+])
+
 app.whenReady().then(() => {
+
+    protocol.handle('simplycode', (request) => {
+        //console.log(request)
+        //console.log(path)
+       
+       
+
+        switch (request.method){
+            case 'PUT':
+                console.log('found a put')
+                let componentName = request.url                
+                if(componentName.endsWith('\/')){
+                    componentName = componentName.substring(0, (componentName.length - 11))
+                }
+                componentName = componentName.split('\/').pop()
+
+                try {
+                    fs.mkdir(('../www/api/components/' + componentName), { recursive: true }, (err) => {
+                        if (err) throw err;
+                      });
+                }catch(e) { 
+                    alert('Failed to save the file !'); 
+                }
+                
+                let pathname = '../www/api/components/' + componentName + '/meta.json';
+                //let contents = '{ "formatted" : "json" }'; // request.body
+                //fs.writeFileSync(pathname, "a string here?!");
+                fs.writeFileSync(pathname, "a string here?!", (err) => {
+                    if (err) throw err;
+                });
+            break
+            case 'GET':
+            default:
+                let path = request.url.replace(/^simplycode:\/\/index.html/, '')
+                if (!path || path === "/") {
+                    path = '/index.html'
+                }
+                const filestuff = fs.readFileSync('../www' + path)
+                return new Response(filestuff, {
+                    // headers: { 'content-type': 'text/html' }
+                })
+            break    
+        }
+
+      
+    })
 
     createWindow()
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
+    
 
 })
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
-
-// /*
-// const http = require('node:http');
-// const net = require('node:net');
-// const { URL } = require('node:url');
-
-// // still need to look into root/lib.config.php to move that content here.
-
-// let request = http.request(options, (response) => {
-//     let data = '<h1>Hello WOrld!</h1>';
-
-//     // A chunk of data has been received
-//     response.on('data', (chunk) => {
-//         data += chunk;
-//     });
-
-//     // The whole response has been received
-//     response.on('end', () => {
-//         console.log(data);
-//     });
-// });
-
-// /*
-// let requestTarget = "";
-// */
-
-// let path = "/api/";
-
-// /*
-// let dirname = filesystem.dirname(path);
-// let filename = filesystem.basename(path);
-// */
-
-
-// try {
-//     const request = http.request();
-//     const requestTarget = request.target.replace(/^\/api/, '');
-//     const path = filesystem.path(requestTarget);
-//     const dirname = filesystem.dirname(path);
-//     const filename = filesystem.basename(path);
-
-//     switch (request.method) {
-//         case 'OPTIONS':
-//             output('ok', 200);
-//             break;
-//         case 'GET':
-//             if (filesystem.exists(`${dirname}/${filename}`)) {
-//                 const result = readRecursive(path);
-//                 output(result, 200);
-//             } else {
-//                 filenotfound(path);
-//             }
-//             break;
-//         case 'POST':
-//             if (filesystem.exists(path)) {
-//                 if (filesystem.is_dir(path)) {
-//                     let exists = true;
-//                     let newId, newFilename;
-//                     while (exists) {
-//                         newId = tagId();
-//                         newFilename = newId;
-//                         exists = filesystem.exists(`${path}${newFilename}`);
-//                     }
-//                     filesystem.put(path, newFilename);
-//                     output(newId, 200);
-//                 }
-//             }
-//             break;
-//         case 'PUT':
-//             if (filesystem.put(dirname, filename)) {
-//                 output('ok', 200);
-//             } else {
-//                 error(new Error('Unexpected result from write', 501));
-//             }
-//             break;
-//         case 'DELETE':
-//             if (filesystem.exists(path) || filesystem.exists(`${dirname}/${filename}`)) {
-//                 deleteRecursive(path);
-//                 output('deleted', 200);
-//             } else {
-//                 filenotfound(path);
-//             }
-//             break;
-//     }
-// } catch (e) {
-//     error(e);
-// }
-
-
-// // Handle errors
-// request.on('error', (error) => {
-//     console.error(error);
-// });
-
-// // End the request
-// request.end();
-
-// */
-// /*
-// function output(data, status) {
-//     http.response(status, data);
-// }
-// */
-// function error(e) {
-//     output("error: 501" + e);
-// }
-
-// function filenotfound(path) {
-//     output("error: 404, message: 'File not found'" + path) ;
-// }
-
-
