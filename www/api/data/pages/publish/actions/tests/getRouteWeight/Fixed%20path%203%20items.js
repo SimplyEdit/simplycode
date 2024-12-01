4aa9ad22236521aca362443a45e0ba12bbd437b0
Yvo Brevoort <yvo@muze.nl>
2024-12-01 10:04:05 +0100

    cleanup url encoded paths

--- www/api/data/pages/publish/actions/tests/getRouteWeight/Fixed%20path%203%20items.js
+++
@@ -1,8 +0,0 @@
-function(assert) {
-  var route = {
-    "route" : "#foo/bar/bla",
-    "code" : ""
-  };
-  var weight = actions.getRouteWeight(route);
-  assert.equal(111, weight, "Path with 3 fixed items weighs 111");
-}
