36b2e370791508984a8910f5c142efbadcd55e0f
Yvo Brevoort <yvo@muze.nl>
2024-12-08 15:29:59 +0100

    add builder-template component

---
+++ components/component-builder-template/actions/build.js
@@ -0,0 +1,29 @@
+function(builderTemplate, data) {
+  var build = builderTemplate;
+  let replacements = {
+    "actions" : editor.transformers.simplyPreviewActions.render(data.actions ?? []),
+    "commands" : editor.transformers.simplyPreviewCommands.render(data.commands ?? []),
+    "dataApi" : editor.transformers.simplyPreviewDataApi.render(data.dataApi ?? []),
+    "rawApi" : editor.transformers.simplyPreviewRawApi.render(data.rawApi ?? []),
+    "routes" : editor.transformers.simplyPreviewRoutes.render(data.routes ?? []),
+    "dataSources" : editor.transformers.simplyPreviewDataSources.render(data.dataSources ?? []),
+    "transformers" : editor.transformers.simplyPreviewTransformers.render(data.transformers ?? []),
+    "sorters" : editor.transformers.simplyPreviewSorters.render(data.sorters ?? []),
+    "pageTemplates" :editor.transformers.simplyPreviewPageTemplates.render(data.pageTemplates ?? []),
+    "pageCss" : editor.transformers.simplyPreviewPageCss.render(data.pageCss ?? []),
+    "componentCss" : editor.transformers.simplyPreviewComponentCss.render(data.componentCss ?? []),
+    "componentTemplates" : editor.transformers.simplyPreviewComponentTemplates.render(data.componentTemplates ?? []),
+    "headHtml": editor.transformers.simplyPreviewHeadHtml.render(data.headHtml ?? []),
+    "footHtml": editor.transformers.simplyPreviewFootHtml.render(data.footHtml ?? []),
+    "bodyHtml" : editor.transformers.simplyPreviewBodyHtml.render(data.bodyHtml ?? [])
+  };
+
+  Object.keys(replacements).forEach(function(replacement) {
+    matches = build.match("(\\s+){{" + replacement + "}}");
+    if (matches) {
+      build = build.replace("{{" + replacement + "}}", replacements[replacement].replace(/\n/g, matches[1]));
+    }
+  });
+
+  return build;
+}
