8cfd6f1f8b1634e404bd83de9ba112c3d400748d
Yvo Brevoort <yvo@muze.nl>
2024-12-18 21:09:15 +0100

    updated version of remote components (same code, brought up to date)

---
+++ components/component-remote/actions/syncRemoteComponent.js
@@ -0,0 +1,37 @@
+function(remote) {
+  let original = {
+    "url" : simplyRawApi.url,
+    "headers" : simplyRawApi.headers
+  };
+  simplyRawApi.url = remote.url;
+  simplyRawApi.headers = {
+    "accept" : "application/json"
+  };
+  let fetchFunctions = {
+    "base-component" : simplyDataApi.getBaseComponent,
+    "component" : simplyDataApi.getComponent,
+    "page" : simplyDataApi.getPage,
+    "page-frame" : simplyDataApi.getPageFrame,
+    "builder" : simplyDataApi.getBuilder
+  };
+  
+  fetchFunctions[remote.type](remote.component)
+  .then(function(parts) {    
+    parts.forEach(function(part) {
+      if (part.id == "meta") {
+        return;
+      }
+      editor.pageData.component.parts[part.id] = JSON.parse(part.contents);
+    });
+  })
+  .then(function() {
+    for (i in original) {
+      simplyRawApi[i] = original[i];
+    }
+  })
+  .catch(function() {
+   for (i in original) {
+      simplyRawApi[i] = original[i];
+    }
+  });
+}
