2d68914c4754443d6fcafa0ba7b82c4db0205dc6
Yvo Brevoort <yvo@muze.nl>
2026-01-23 22:22:34 +0100

    add functions to get module base-components, components and pages

---
+++ pages/imports/dataApi/listSimplyModuleComponents.js
@@ -0,0 +1,29 @@
+function() {
+  return simplyRawApi.get("simply-modules")
+    .then(function(response) {
+    if (response.status === 200) {
+      return response.json();
+    }
+    throw new Error("listSimplyModules failed", response.status);
+  })
+    .then(function(simplyModules) {
+    let components = [];
+    simplyModules.forEach(function(moduleGroup) {
+      moduleGroup.contents.forEach(function(module) {
+        module.contents.forEach(function(moduleComponentCategory) {
+          switch(moduleComponentCategory.id) {
+            case "components":
+              moduleComponentCategory.contents.forEach(function(component) {
+                simplyDataApi.mergeComponent(component.contents);
+                component.description = component.contents.description;
+                component.baseType = 'component';
+                components.push(component);
+              });
+            break;
+          }
+        });
+      });
+    });
+    return components;
+  });
+}
